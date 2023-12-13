ff049756080d8e88cc2231bb3d82ea50c8230b89
iichanize <iichanize.irl@gmail.com>
2023-12-13 17:02:22 +0900

    first commit

---
+++ vueApp/vue-sample/src/components/Sudoku.vue
@@ -0,0 +1,59 @@
+<template>
+  <table id="table">
+    <tr v-for="(lines, index1) in field" v-bind:key="index1">
+      <td v-for="(num, index2) in lines" v-bind:key="index2">
+        <button name="box" @click="countUp(index1, index2)">{{ num }}</button>
+      </td>
+    </tr>
+  </table>
+</template>
+<script lang="ts">
+  import { defineComponent, ref } from "vue";
+
+  export default defineComponent({
+    name: "HelloWorld",
+    props: {
+      msg: String,
+    },
+    setup(props, ctx) {
+      let field = ref([
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+      ]);
+      const countUp = (index1, index2) => {
+        console.log("push");
+        let num = field.value[index1][index2] + 1;
+        field.value[index1][index2] = num > 9 ? num % 10 : num;
+        return;
+      };
+      return {
+        countUp,
+        field,
+      };
+    },
+  });
+</script>
+<style>
+  #table {
+    margin: 0 auto;
+    border-collapse: collapse;
+    border: 3px solid #ccc;
+  }
+
+  #table td {
+    border: 1px solid #ccc;
+    height: 40px;
+    width: 40px;
+    text-align: center;
+    vertical-align: middle;
+    font-size: 32px;
+    cursor: pointer;
+  }
+</style>
---
+++ vueApp/vue-sample/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>Sudoku</h1>
+  </div>
+  <div
+    class="container mx-auto"
+    id="id_container"
+    style="text-align: center; width: 95%; height: 70%; margin: 0 auto"
+  >
+    <Sudoku />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+  import Sudoku from "@/components/Sudoku.vue";
+
+  export default defineComponent({
+    name: "AboutView",
+    components: {
+      Sudoku,
+    },
+  });
+</script>
