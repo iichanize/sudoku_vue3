d27418a42faa00a5e8acebf50ae89505d2763a35
iichanize <iichanize.irl@gmail.com>
2024-02-07 12:19:45 +0900

    HOME画面削除

--- vueApp/vue-sample/src/router/index.ts
+++ vueApp/vue-sample/src/router/index.ts
@@ -4,12 +4,7 @@ import HomeView from "../views/HomeView.vue";
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    name: "home",
-    component: HomeView,
-  },
-  {
-    path: "/about",
-    name: "about",
+    name: "content",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
